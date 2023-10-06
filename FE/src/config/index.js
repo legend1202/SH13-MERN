4d21692935771ed07f0b54cf269e25a482e67933
legend <phoenixdev0211@gmail.com>
2023-10-06 04:28:54 -0500

    update api url

--- FE/src/config/index.js
+++ FE/src/config/index.js
@@ -1,5 +1,5 @@
-export const API_URL = `http://beehives.grwth.hk/api`;
-export const HOST_URL = `http://${window.location.hostname}:8081`;
+export const API_URL = `https://beehives.grwth.hk/api`;
+export const HOST_URL = `https://${window.location.hostname}:8081`;
 
 export const convertApiUrl = (url, param="") => {
   const lng = window.localStorage.getItem("i18nextLng").slice(0, 2);
